2027afe5332ca49e1299a5746b2dffdfa61e586b
Anant Narayan <anant.n@ahduni.edu.in>
2025-08-29 11:58:30 +0530

    Initial code

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+// Hashtbl for search
+// AVL tree? for add/remove
+// Queues for borrow (this is ez)
+// Branch: graph/tree with A*
+// Budget: ????
+
+public class Main
+{
+ public static void main(String[] args) 
+ {
+   Scanner sc = new Scanner(System.in);
+   int option = 6;
+   System.out.print("Welcome to the library. Features available are as follows:\n1. Book Search\n2. Manage Books\n3. Borrow book/check borrow records\n4. Branch transport\n5. Book purchase calculator\nType in the feature number to go to that feature, or anything else to exit.\n");
+   while(true){
+    System.out.print("Select feature: ");
+    try{
+     option = sc.nextInt();
+    }
+    catch(Exception e) {
+     System.out.println("Have a nice day!");
+     sc.close();
+     System.exit(0);  
+    }
+    switch(option){
+     case 1:
+        System.out.println("Book Search");             
+        break;
+     case 2:
+        System.out.println("Manage Books");             
+        break;
+     case 3:
+        System.out.println("Borrow Book/Check Borrow Records");             
+        break;
+     case 4:
+        System.out.println("Book transport from branch");             
+        break;
+     case 5:
+        System.out.println("Calculate optimal purchase from budget");             
+        break;
+     default:
+        System.out.println("Have a nice day!");
+        sc.close();
+        System.exit(0);  
+    }
+  }
+ }
+}
+
